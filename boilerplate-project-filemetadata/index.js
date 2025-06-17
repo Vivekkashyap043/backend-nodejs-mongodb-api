4f017692c4aa9e48d306332679e8051e14d321ec
vivek <vivekkashyap043@gmail.com>
2025-06-17 10:55:02 +0530

    file analsey

---
+++ boilerplate-project-filemetadata/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var cors = require('cors');
+var multer = require('multer');
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Multer setup
+const upload = multer({ dest: 'uploads/' });
+
+// Route for file upload
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) return res.status(400).json({ error: 'No file uploaded' });
+
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
